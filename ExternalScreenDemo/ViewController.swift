a43ece220c3c8dd81e89da340db2e3e6efe283e1
Andrew Muncey <andrewmuncey@andrews-macbook-pro-2013.local>
2019-02-03 22:03:11 +0000

    Initial Commit

---
+++ ExternalScreenDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExternalScreenDemo
+//
+//  Created by Andrew Muncey on 03/02/2019.
+//  Copyright © 2019 Andrew Muncey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
